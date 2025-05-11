66a27831c1e3669d43a034c6363c212917d13d16
DelusionalDebugger <rogid9607r@gmail.com>
2025-05-11 12:21:31 +0100

    Enemy now finds a new location to move to
    Two floating point numbers are prob ot gonna be the same so using distance to will calc diist until target is reached, so using this allows entity to know when to move to another location

--- Collection/Enemies/AI/Enemies/Find point enemy/find_point_enemy.gd
+++ Collection/Enemies/AI/Enemies/Find point enemy/find_point_enemy.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
-@onready var pointTracker: RayCast2D = $PointTracker
-
 var chosenLocation : Vector2
-var movementVelocity : int = 30
+var movementVelocity : int = 90
 var targetLocation : Vector2
 
 func _ready() -> void:
 	randomize()
-	targetLocation = Vector2(randi_range(-2000,2000),randi_range(-2000,2000))	#Can replace these ranges with screen or scene size needed 
+	chosenLocation = Vector2(randi_range(-2000,2000),randi_range(-2000,2000))	#Can replace these ranges with screen or scene size needed 
 
 func _physics_process(delta: float) -> void:
-	pointTracker.look_at(chosenLocation)
 	targetLocation = (chosenLocation - global_position).normalized()
-	position += movementVelocity * delta * targetLocation
+	global_position += movementVelocity * delta * targetLocation
+	print(chosenLocation)
 	
-	if targetLocation == Vector2(0,0):
-		targetLocation = Vector2(randi_range(-2000,2000),randi_range(-2000,2000))
+	if global_position.distance_to(chosenLocation) < 3.0: #If distance to destination is less than 3.0
+		randomize()
+		chosenLocation = Vector2(randi_range(-2000,2000),randi_range(-2000,2000))
