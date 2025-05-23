d422a74627a02e36f29c0865d1f9448b7ea6fe50
DelusionalDebugger <rogid9607r@gmail.com>
2025-05-23 18:43:41 +0100

    setting up scene to demonstrate find and point enemy and scene to navigate all 2D sidescroller modular mechanic scenes in a menu

--- Collection/Enemies/AI/Enemies/Find point enemy/find_point_enemy.gd
+++ Collection/Enemies/AI/Enemies/Find point enemy/find_point_enemy.gd
@@ -10,7 +10,7 @@ var player = null #Player in the scene will be stored here
 
 func _ready() -> void:
 	randomize()
-	chosenLocation = Vector2(randi_range(-2000,2000),randi_range(-2000,2000))	#Can replace these ranges with screen or scene size needed 
+	chosenLocation = Vector2(randi_range(-500,500),randi_range(-500,500))	#Can replace these ranges with screen or scene size needed and the integers with a constant to change range easier
 	player = get_tree().get_first_node_in_group("Player")
 
 
@@ -25,7 +25,7 @@ func randomMovement(delta: float):
 	
 		if global_position.distance_to(chosenLocation) < 3.0: #If distance to destination is less than 3.0
 			randomize()
-			chosenLocation = Vector2(randi_range(-2000,2000),randi_range(-2000,2000))
+			chosenLocation = Vector2(randi_range(-500,500),randi_range(-500,500))
 
 
 func handlePlayer(player:CharacterBody2D , delta:float):
@@ -38,7 +38,7 @@ func handlePlayer(player:CharacterBody2D , delta:float):
 		if playerTracker.is_colliding(): #If raycast collides
 			var collisionResult = playerTracker.get_collider()
 			if collisionResult == player:
-				var trackPlayerLocation = (player.global_position - global_position).normalized() #Replace with damaging player logic
+				var trackPlayerLocation = (player.global_position - global_position).normalized() #Has tracking logic, should add attacking player logic
 				position += trackPlayerLocation * delta * movementVelocity
 			else:
 				return
